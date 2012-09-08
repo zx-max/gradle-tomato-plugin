d3e2d30b0d337a843de948abb5920907768299e0
max <MAX@PcVista.(none)>
2012-09-08 15:49:33 +0200

    Explain order of execution of the acions in a single task.

--- src/test/groovy/org/gradle/TomatoPluginTest.groovy
+++ src/test/groovy/org/gradle/TomatoPluginTest.groovy
@@ -12,9 +12,35 @@ import static org.junit.Assert.*;
 import java.lang.reflect.Method;
 
 class TomatoPluginTest {
+/*
+ Explain the order of execution of the acions in a single task.
+ 
+org.gradle.api.internal.project.taskfactory.AnnotationProcessingTaskFactory.createTask(ProjectInternal project, Map<String, ?> args){
+...
+        for (Action<Task> action : actions) {
+            task.doFirst(action);
+...
+		
+			
+org.gradle.api.internal.AbstractTask {
+...
+private List<Action<? super Task>> actions = new ArrayList<Action<? super Task>>();			
+..
+public Task doFirst(Closure action) {
+...
+ actions.add(0, wrap(action));
+... 	
+ */
+	
     @Test
     public void tomatoPluginAddsCreateTaskToProject() {
-
+		Method[] methods = TomatoTask.class.getDeclaredMethods();
+		println methods.collect {it.name}
+		for (m in methods){
+			if( m.getAnnotation(TaskAction.class) != null)
+			println m.name
+		}
+		
         Project project = ProjectBuilder.builder().build()
         project.apply plugin: 'tomato'
 
